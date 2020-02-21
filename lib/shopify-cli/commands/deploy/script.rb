5d78ac4e07e20d5fc7bcd5724af1453aea07ae2a
Jacob Steves <jacob.steves@shopify.com>
2020-02-21 16:45:47 -0500

    Improve deploy help text & error messages

--- lib/shopify-cli/commands/deploy/script.rb
+++ lib/shopify-cli/commands/deploy/script.rb
@@ -4,22 +4,24 @@ module ShopifyCli
   module Commands
     class Deploy
       class Script < ShopifyCli::Command
-        CMD_DESCRIPTION = "Deploy a script to the extension platform"
-        CMD_USAGE = "deploy <API Key>"
+        CMD_DESCRIPTION = "Build the script and deploy it to the app."
+        CMD_USAGE = "deploy --API_key=<API_key>"
 
-        FAILED_TO_BUILD_MESSAGE = "Failed to build"
-        DEPLOY_SUCCEEDED_MSG = "{{v}} %{extension_point} script %{script_name} " \
-        "is deployed to app (API_KEY: {{green:%{api_key}}})"
         BUILDING_MSG = "Building"
         DEPLOYING_MSG = "Deploying"
         BUILT_MSG = "Built"
         DEPLOYED_MSG = "Deployed"
 
         INVALID_EXTENSION_POINT = "Invalid extension point %{extension_point}"
+        OPERATION_SUCCESS_MESSAGE = "Your script is deployed to the app (API key: {{green:%{api_key}}})."
+        OPERATION_FAILED_MESSAGE = "The script didn't deploy."
+        INVALID_EXTENSION_POINT_MESSAGE = "The extension point option is not correct."
         SCRIPT_NOT_FOUND = "Could not find script %{script_name} for extension point %{extension_point}"
         SCRIPT_REDEPLOY_ERROR = "{{x}} {{red:Error}}\nThe script didn't deploy. A script with this extension "\
                                   "point already exists on app (%{api_key}). Use the --force option to overwrite "\
                                   "the existing script with this new script."
+        DEPLOY_BUILD_FAILURE_MESSAGE = "Something went wrong while building the script."
+        DEPLOY_HELP_SUGGESTION = "Correct the errors and try again."
 
         options do |parser, flags|
           parser.on('--api_key=APIKEY') { |t| flags[:api_key] = t }
@@ -42,22 +44,14 @@ module ShopifyCli
           dep_manager = ScriptModule::Infrastructure::DependencyManager.for(@ctx, script_name, language)
           dep_manager.install unless dep_manager.installed?
 
-          ShopifyCli::UI::StrictSpinner.spin(BUILDING_MSG) do |spinner|
-            ScriptModule::Application::Build.call(language, extension_point_type, script_name)
-            spinner.update_title(BUILT_MSG)
-          end
+          build_script(language, extension_point_type, script_name)
 
           ShopifyCli::UI::StrictSpinner.spin(DEPLOYING_MSG) do |spinner|
             deploy_script(language, extension_point_type, script_name, api_key)
             spinner.update_title(DEPLOYED_MSG)
           end
 
-          @ctx.puts(
-            format(
-              DEPLOY_SUCCEEDED_MSG, script_name: script_name, extension_point: extension_point_type, api_key: api_key
-            )
-          )
-
+          @ctx.puts("{{v}} #{format(OPERATION_SUCCESS_MESSAGE, api_key: api_key)}")
         rescue ScriptModule::Infrastructure::ScriptRedeployError
           @ctx.puts(format(SCRIPT_REDEPLOY_ERROR, api_key: api_key))
         rescue ScriptModule::Domain::ScriptNotFoundError
@@ -81,6 +75,30 @@ module ShopifyCli
           ScriptModule::Application::Deploy.call(
             @ctx, language, extension_point_type, script_name, api_key, options.flags[:force]
           )
+        end
+
+        def build_script(language, extension_point_type, script_name)
+          success = CLI::UI::Frame.open('Building') do
+            begin
+              CLI::UI::Progress.progress do |bar|
+                bar.tick(set_percent: 0)
+                ScriptModule::Application::Build.call(language, extension_point_type, script_name)
+                bar.tick(set_percent: 1.0)
+                true
+              end
+            rescue StandardError => e
+              @ctx.puts("\n{{red:#{e.message}}}")
+              false
+            end
+          end
+
+          unless success
+            ShopifyCli::UI::ErrorHandler.display_and_raise(
+              failed_op: OPERATION_FAILED_MESSAGE,
+              cause_of_error: DEPLOY_BUILD_FAILURE_MESSAGE,
+              help_suggestion: DEPLOY_HELP_SUGGESTION
+            )
+          end
         end
       end
     end
--- test/shopify-cli/commands/deploy/script_test.rb
+++ test/shopify-cli/commands/deploy/script_test.rb
@@ -10,6 +10,9 @@ module ShopifyCli
           super
           @cmd = ShopifyCli::Commands::Deploy::Script
           @cmd.ctx = @context
+          @language = 'ts'
+          @extension_point_type = 'discount'
+          @script_name = 'script name'
         end
 
         def test_deploy_is_forced_when_flag_is_present
@@ -22,17 +25,35 @@ module ShopifyCli
           expect_deploy_with_force(cmd, nil)
         end
 
+        def test_build_script_calls_error_handler_when_build_fails
+          cmd = new_command_with_options
+          err_msg = 'some error message'
+
+          ScriptModule::Application::Build.stubs(:call).raises(StandardError, err_msg)
+          io = capture_io do
+            ShopifyCli::UI::ErrorHandler.expects(:display_and_raise)
+            cmd.send(:build_script, @language, @extension_point_type, @script_name)
+          end
+          output = io.join
+          assert_match(err_msg, output)
+        end
+
+        def test_build_script_returns_normal_when_build_succeeds
+          cmd = new_command_with_options
+          ScriptModule::Application::Build.stubs(:call).returns(true)
+          capture_io do
+            ShopifyCli::UI::ErrorHandler.expects(:display_and_raise).never
+            cmd.send(:build_script, @language, @extension_point_type, @script_name)
+          end
+        end
+
         private
 
         def expect_deploy_with_force(cmd, force)
-          language = 'ts'
-          extension_point_type = 'discount'
-          script_name = 'script'
-          api_key = 'key'
           ScriptModule::Application::Deploy.expects(:call).with(
-            @context, language, extension_point_type, script_name, api_key, force
+            @context, @language, @extension_point_type, @script_name, @api_key, force
           )
-          cmd.send(:deploy_script, language, extension_point_type, script_name, api_key)
+          cmd.send(:deploy_script, @language, @extension_point_type, @script_name, @api_key)
         end
 
         def new_command_with_options(*options)
