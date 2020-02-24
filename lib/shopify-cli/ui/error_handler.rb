66e724466869902c0b6e32257ba2de2c4ccbdada
Erin Ren <erin.ren@shopify.com>
2020-02-24 10:19:58 -0500

    Ensure sentences have 1 space in between

--- lib/shopify-cli/ui/error_handler.rb
+++ lib/shopify-cli/ui/error_handler.rb
@@ -6,8 +6,8 @@ module ShopifyCli
       def self.display_and_raise(failed_op:, cause_of_error:, help_suggestion:)
         $stderr.puts(CLI::UI.fmt("{{red:{{x}} Error}}"))
         full_msg = failed_op ? failed_op.dup : ""
-        full_msg << "  #{cause_of_error}" if cause_of_error
-        full_msg << "  #{help_suggestion}" if help_suggestion
+        full_msg << " #{cause_of_error}" if cause_of_error
+        full_msg << " #{help_suggestion}" if help_suggestion
         $stderr.puts(full_msg.strip)
         raise(ShopifyCli::AbortSilent)
       end
--- test/shopify-cli/ui/error_handler_test.rb
+++ test/shopify-cli/ui/error_handler_test.rb
@@ -20,7 +20,7 @@ describe ShopifyCli::UI::ErrorHandler do
             subject
           end
         end
-        expected_err_msg = "\e[0;31m✗ Error\e[0m#{failed_op}  #{cause_of_error}  #{help_suggestion}"
+        expected_err_msg = "\e[0;31m✗ Error\e[0m#{failed_op} #{cause_of_error} #{help_suggestion}"
         assert_equal(expected_err_msg, err_msg[1])
       end
     end
@@ -34,7 +34,7 @@ describe ShopifyCli::UI::ErrorHandler do
           end
         end
 
-        expected_err_msg = "\e[0;31m✗ Error\e[0m#{cause_of_error}  #{help_suggestion}"
+        expected_err_msg = "\e[0;31m✗ Error\e[0m#{cause_of_error} #{help_suggestion}"
         assert_equal(expected_err_msg, err_msg[1])
       end
     end
@@ -48,7 +48,7 @@ describe ShopifyCli::UI::ErrorHandler do
           end
         end
 
-        expected_err_msg = "\e[0;31m✗ Error\e[0m#{failed_op}  #{help_suggestion}"
+        expected_err_msg = "\e[0;31m✗ Error\e[0m#{failed_op} #{help_suggestion}"
         assert_equal(expected_err_msg, err_msg[1])
       end
     end
@@ -62,7 +62,7 @@ describe ShopifyCli::UI::ErrorHandler do
           end
         end
 
-        expected_err_msg = "\e[0;31m✗ Error\e[0m#{failed_op}  #{cause_of_error}"
+        expected_err_msg = "\e[0;31m✗ Error\e[0m#{failed_op} #{cause_of_error}"
         assert_equal(expected_err_msg, err_msg[1])
       end
     end
