53aa765d5267c9f80d708eddffac23a9483a7515
Erin Ren <erin.ren@shopify.com>
2020-02-24 07:16:55 -0500

    Detect test failures

--- lib/shopify-cli/commands/test.rb
+++ lib/shopify-cli/commands/test.rb
@@ -6,9 +6,14 @@ module ShopifyCli
       available_in_contexts 'test', [:script]
 
       CMD_DESCRIPTION = "Runs unit tests"
-      RUNNING_MSG = "Running tests..."
+      RUNNING_MSG = "Running tests"
       CMD_USAGE = "test"
-      OPERATION_FAILED_MESSAGE = "The tests weren't run."
+      OPERATION_FAILED_MESSAGE = "The tests weren't run or ran with failures."
+      OPERATION_SUCCESS_MESSAGE = "Your tests finished running."
+      TEST_HELP_SUGGESTION = 'Correct the errors and try again.'
+
+      private_constant :CMD_DESCRIPTION, :RUNNING_MSG, :CMD_USAGE, :OPERATION_FAILED_MESSAGE,
+        :OPERATION_SUCCESS_MESSAGE, :TEST_HELP_SUGGESTION
 
       def call(_args, _name)
         project = ShopifyCli::ScriptModule::ScriptProject.current
@@ -28,8 +33,17 @@ module ShopifyCli
         end
 
         @ctx.setenv("FORCE_COLOR", "1") # without this, aspect output is not in color :(
-        CLI::UI::Frame.open("Running tests") do
+        result = CLI::UI::Frame.open(RUNNING_MSG) do
           ScriptModule::Application::Test.call(@ctx, language, extension_point_type, script_name)
+        end
+        if result.success?
+          @ctx.puts("{{v}} #{OPERATION_SUCCESS_MESSAGE}")
+        else
+          ShopifyCli::UI::ErrorHandler.display_and_raise(
+            failed_op: OPERATION_FAILED_MESSAGE,
+            cause_of_error: nil,
+            help_suggestion: TEST_HELP_SUGGESTION
+          )
         end
       rescue ShopifyCli::ScriptModule::InvalidScriptProjectContextError
         ShopifyCli::UI::ErrorHandler.display_and_raise(ShopifyCli::Project.error_messages(OPERATION_FAILED_MESSAGE))
