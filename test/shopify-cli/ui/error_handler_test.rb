7aa4d4207172b7a6323896b94e18fcf6b8781514
Jacob Steves <jacob.steves@shopify.com>
2020-02-27 07:32:27 -0500

    Fix error handler tests

--- test/shopify-cli/ui/error_handler_test.rb
+++ test/shopify-cli/ui/error_handler_test.rb
@@ -15,55 +15,44 @@ describe ShopifyCli::UI::ErrorHandler do
 
     describe "when failed operation message, cause of error, and help suggestion are all provided" do
       it "should abort with the cause of error and help suggestion" do
-        err_msg = capture_io do
-          assert_raises(ShopifyCli::AbortSilent) do
-            subject
-          end
+        $stderr.expects(:puts).with("\e[0;31m✗ Error\e[0m")
+        $stderr.expects(:puts).with("#{failed_op} #{cause_of_error} #{help_suggestion}")
+        assert_raises(ShopifyCli::AbortSilent) do
+          subject
         end
-        expected_err_msg = "\e[0;31m✗ Error\e[0m#{failed_op} #{cause_of_error} #{help_suggestion}"
-        assert_equal(expected_err_msg, err_msg[1])
       end
     end
 
     describe "when failed operation message is missing" do
       let(:failed_op) { nil }
       it "should abort with the cause of error and help suggestion" do
-        err_msg = capture_io do
-          assert_raises(ShopifyCli::AbortSilent) do
-            subject
-          end
+        $stderr.expects(:puts).with("\e[0;31m✗ Error\e[0m")
+        $stderr.expects(:puts).with("#{cause_of_error} #{help_suggestion}")
+        assert_raises(ShopifyCli::AbortSilent) do
+          subject
         end
-
-        expected_err_msg = "\e[0;31m✗ Error\e[0m#{cause_of_error} #{help_suggestion}"
-        assert_equal(expected_err_msg, err_msg[1])
       end
     end
 
     describe "when cause of error is missing" do
       let(:cause_of_error) { nil }
       it "should abort with the failed operation message and help suggestion" do
-        err_msg = capture_io do
-          assert_raises(ShopifyCli::AbortSilent) do
-            subject
-          end
+        $stderr.expects(:puts).with("\e[0;31m✗ Error\e[0m")
+        $stderr.expects(:puts).with("#{failed_op} #{help_suggestion}")
+        assert_raises(ShopifyCli::AbortSilent) do
+          subject
         end
-
-        expected_err_msg = "\e[0;31m✗ Error\e[0m#{failed_op} #{help_suggestion}"
-        assert_equal(expected_err_msg, err_msg[1])
       end
     end
 
     describe "when help suggestion is missing" do
       let(:help_suggestion) { nil }
       it "should abort with the failed operation message and cause of error" do
-        err_msg = capture_io do
-          assert_raises(ShopifyCli::AbortSilent) do
-            subject
-          end
+        $stderr.expects(:puts).with("\e[0;31m✗ Error\e[0m")
+        $stderr.expects(:puts).with("#{failed_op} #{cause_of_error}")
+        assert_raises(ShopifyCli::AbortSilent) do
+          subject
         end
-
-        expected_err_msg = "\e[0;31m✗ Error\e[0m#{failed_op} #{cause_of_error}"
-        assert_equal(expected_err_msg, err_msg[1])
       end
     end
   end
