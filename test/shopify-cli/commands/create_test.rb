c5404db9fc0cdfe53a777f20e6148787e1a94635
Jacob Steves <jacob.steves@shopify.com>
2020-03-02 11:31:57 -0500

    Revert "hide create script command behind an env var" (#127)
    This reverts commit a31d9f842b341df1eb8ad8e99dc15a33fb3183aa.

--- test/shopify-cli/commands/create_test.rb
+++ test/shopify-cli/commands/create_test.rb
@@ -3,12 +3,11 @@ require 'test_helper'
 module ShopifyCli
   module Commands
     class CreateTest < MiniTest::Test
-      include TestHelpers::Constants
-
       def setup
         super
-        ShopifyCli::Project.stubs(:current_context).returns(:top_level)
-        load_cmd
+        @cmd = ShopifyCli::Commands::Create
+        @cmd.ctx = @context
+        @cmd_name = 'create'
       end
 
       def test_without_arguments_calls_help
@@ -22,22 +21,10 @@ module ShopifyCli
         @cmd.call(['app', 'new-app'], @cmd_name)
       end
 
-      def test_with_create_script_with_env_var_set
-        ENV.stubs(:[]).with('SCRIPTS_PLATFORM').returns('true')
-        load_cmd
-
+      def test_with_create_script
         Create::Script.any_instance.expects(:call)
           .with(['discount', 'new-script'], 'script')
         @cmd.call(['script', 'discount', 'new-script'], @cmd_name)
-      end
-
-      def test_create_script_is_hidden_unless_env_is_set
-        ENV.stubs(:[]).with('SCRIPTS_PLATFORM').returns(nil)
-        load_cmd
-
-        Create::Script.any_instance.expects(:call).never
-        @cmd.call(['script', 'discount', 'new-script'], @cmd_name)
-        assert_nil(@cmd.subcommand_registry.lookup_command('script').first)
       end
 
       def test_with_create_project_returns_warning
@@ -45,24 +32,6 @@ module ShopifyCli
           @cmd.call(['project', 'new-app'], @cmd_name)
         end
         assert_match('shopify create app', io.join)
-      end
-
-      private
-
-      def load_cmd
-        reload_class
-        @cmd = ShopifyCli::Commands::Create
-        @cmd.ctx = @context
-        @cmd_name = 'create'
-      end
-
-      def reload_class
-        ignore_constant_redefined_warnings do
-          ShopifyCli::Commands.send(:remove_const, :Create)
-          load('shopify-cli/commands/create.rb')
-          load('shopify-cli/commands/create/app.rb')
-          load('shopify-cli/commands/create/script.rb')
-        end
       end
     end
   end
